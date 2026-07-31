dcf04ee96425ee2b0a00454ba3216cb1e616ff46
Yeleke <yeleke.umbetov@gmail.com>
2026-07-31 15:37:57 +0500

    changed hardcoded test data to a dataclass with dynamically creating data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
 import pytest
 from playwright.sync_api import Page
 from pages.home_page import HomePage
+from utils.data_models import AddressData
 
 @pytest.fixture
 def home_page(page: Page) -> HomePage:
     home = HomePage(page)
     home.navigate_to("https://automationexercise.com")
     return home
+
+@pytest.fixture
+def random_address():
+    return AddressData.generate()
 
