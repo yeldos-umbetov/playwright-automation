b016fa24a1e607dcc1ac8e7955a51a4ef9f275c7
Yeleke <yeleke.umbetov@gmail.com>
2026-09-07 17:32:11 +0500

    test 3 is ready

--- pages/login_page.py
+++ pages/login_page.py
@@ -4,17 +4,18 @@ from pages.base_page import BasePage
 class LoginPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
-        """signup locators"""
+        # signup locators
         self.signup_header = page.get_by_role("heading", name = "New User Signup")
         self.signup_name_input = page.get_by_placeholder("Name")
         self.signup_email_input = page.locator(".signup-form input[name='email']")
         self.signup_button = page.get_by_role("button", name = "Signup")
-        """login locators"""
+        # login locators
         self.login_header = page.get_by_role("heading", name = "Login to your account")
         self.login_email_input = page.get_by_test_id("login-email")
         self.login_password_input = page.get_by_test_id("login-password")
         self.login_button = page.get_by_role("button", name = "Login")
 
+        self.invalid_login_text = page.get_by_text("Your email or password is incorrect!")
     def submit_initial_signup(self, name: str, email: str):
         self.signup_name_input.fill(name)
         self.signup_email_input.fill(email)
